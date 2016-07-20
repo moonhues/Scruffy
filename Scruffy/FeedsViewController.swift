02224fe19eacf94015fb9be360545fa2aebbe1f4
moonhues <veron@vt.sg>
2016-07-21 07:52:09 +0800

    feeds scroll done

--- Scruffy/FeedsViewController.swift
+++ Scruffy/FeedsViewController.swift
@@ -14,26 +14,99 @@ class FeedsViewController: UIViewController {
      // MARK: Properties
 
     @IBOutlet weak var petImageView: UIImageView!
+    @IBOutlet weak var petNameLabel: UILabel!
+    
+    var arrayOfPets: [NSDictionary] = []
+    
+    var swipeLeftHandler: UISwipeGestureRecognizer?
+    var swipeRightHandler: UISwipeGestureRecognizer?
+    
+    var currentPosition: Int = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         // view.addSubview(<#T##view: UIView##UIView#>)
+        
+        let newPet = NSMutableDictionary()
+        newPet["name"] = "Afya"
+        newPet["image"] = "Afya"
+        arrayOfPets.append(newPet)
+        
+        let newPet2 = NSMutableDictionary()
+        newPet2["name"] = "Benny"
+        newPet2["image"] = "Benny"
+        arrayOfPets.append(newPet2)
+        
+        let newPet3 = NSMutableDictionary()
+        newPet3["name"] = "Anita"
+        newPet3["image"] = "Anita"
+        arrayOfPets.append(newPet3)
+        
+        let newPet4 = NSMutableDictionary()
+        newPet4["name"] = "Chanel"
+        newPet4["image"] = "Chanel"
+        arrayOfPets.append(newPet4)
+        
+       petNameLabel.text = newPet["name"] as? String
+       petImageView.image = UIImage(named: newPet["image"] as! String)
+        
+        
+        //Swipe Gesture
+        swipeLeftHandler = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
+        swipeLeftHandler?.direction = .Left
+        petImageView.addGestureRecognizer(swipeLeftHandler!)
+        
+        swipeRightHandler = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
+        swipeRightHandler?.direction = .Right
+        petImageView.addGestureRecognizer(swipeRightHandler!)
     }
-
-    // MARK: Actions
-
-    @IBOutlet weak var petNameLabel: UILabel!
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func swipeHandler(gesture: UISwipeGestureRecognizer) {
+        
+        if gesture == swipeLeftHandler {
+            
+            print("swiped left")
+            
+            //goes forward
+            if currentPosition < arrayOfPets.count - 1 {
+                currentPosition = currentPosition + 1
+                
+                let pet = arrayOfPets[currentPosition]
+                reloadView(pet)
+            }
+        }
+        
+        if gesture == swipeRightHandler {
+            
+            print("swiped right")
+            
+            //goes backward
+            if currentPosition > 0 {
+                currentPosition = currentPosition - 1
+                
+                let pet = arrayOfPets[currentPosition]
+                reloadView(pet)
+            }
+        }
     }
-    */
+    
+    func reloadView(petDictionary: NSDictionary) {
+        
+        print("currentposition: \(currentPosition)")
+        
+        petNameLabel.text = petDictionary["name"] as? String
+        petImageView.image = UIImage(named: petDictionary["image"] as! String)
+    }
 
+    
+    
 }
+
+
+
+
+
