94633339b35674cd6ec010936a6573a09d554e4e
moonhues <veron@vt.sg>
2016-07-21 07:32:41 +0800

    uploadpost

--- Scruffy/PostIndexViewController.swift
+++ Scruffy/PostIndexViewController.swift
@@ -7,44 +7,107 @@
 //
 
 import UIKit
+import ConvenienceKit
+import Parse
 
-class PostIndexViewController: UIViewController {
+class PostIndexViewController: UIViewController, TimelineComponentTarget {
 
     @IBOutlet weak var tableView: UITableView!
-   
+    
+    var timelineComponent: TimelineComponent <Post, PostIndexViewController>!
+    
+    let defaultRange = 0...4
+    let additionalRangeSize = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        timelineComponent = TimelineComponent(target: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // 1
-        if let identifier = segue.identifier {
-            // 2
-            if identifier == "displayEditProfile" {
-                // 3
-                print("Transitioning to the Edit Post View Controller")
+    func loadInRange(range: Range<Int>, completionBlock: ([Post]?) -> Void) {
+        ParseHelper.timelineRequestForCurrentUser(range) {
+            (result: [PFObject]?, error: NSError?) -> Void in
+            if let error = error {
+                ErrorHandling.defaultErrorHandler(error)
             }
+            
+            let posts = result as? [Post] ?? []
+            completionBlock(posts)
         }
     }
     
+    // MARK: UIActionSheets
     
+    func showActionSheetForPost(post: Post) {
+        if (post.user == PFUser.currentUser()) {
+            showDeleteActionSheetForPost(post)
+        }
+    }
+    
+    func showDeleteActionSheetForPost(post: Post) {
+        let alertController = UIAlertController(title: nil, message: "Do you want to delete this post?", preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let destroyAction = UIAlertAction(title: "Delete", style: .Destructive) { (action) in
+            post.deleteInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                if (success) {
+                    self.timelineComponent.removeObject(post)
+                } else {
+                    // restore old state
+                    self.timelineComponent.refresh(self)
+                }
+            })
+        }
+        alertController.addAction(destroyAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+}
+
+extension PostIndexViewController: UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.timelineComponent.content.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        
+        let post = timelineComponent.content[indexPath.section]
+        post.downloadImage()
+        cell.post = post
+        cell.timeline = self
+        
+        return cell
+    }
+}
+
+extension PostIndexViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        timelineComponent.targetWillDisplayEntry(indexPath.section)
+    }
+    
+    /*
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        //let headerCell = tableView.dequeueReusableCellWithIdentifier("PostHeader") as! PostSectionHeaderView
+        
+        let post = self.timelineComponent.content[section]
+       // headerCell.post = post
+        
+        return headerCell
+    }*/
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
