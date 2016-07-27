7e0f376b2e4d28098b84e3e4cf61b6a70edb3214
moonhues <veron@vt.sg>
2016-07-27 18:47:47 +0800

    Add, Edit, Profile and Refresh working

--- Scruffy/UserInfoViewController.swift
+++ Scruffy/UserInfoViewController.swift
@@ -18,12 +18,11 @@ class UserInfoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: false)
     }
-    
-    
     
     // MARK: - Navigation
     
